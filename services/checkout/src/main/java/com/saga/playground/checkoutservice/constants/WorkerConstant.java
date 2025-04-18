3159dd2974f27d67586a0321ecb3cc0891a30050
TrungTho <41388666+TrungTho@users.noreply.github.com>
2025-04-18 16:12:36 +0700

    feat(checkout): WorkerTriggerTask (#17)

--- services/checkout/src/main/java/com/saga/playground/checkoutservice/constants/WorkerConstant.java
+++ services/checkout/src/main/java/com/saga/playground/checkoutservice/constants/WorkerConstant.java
@@ -13,6 +13,8 @@ public final class WorkerConstant {
 
     public static final int WORKER_CHECKOUT_DELAY_MILLISECONDS = 1_000;
 
+    public static final int WORKER_CHECKOUT_INTERVAL_MILLISECONDS = 5_000;
+
     private WorkerConstant() {
     }
 }
