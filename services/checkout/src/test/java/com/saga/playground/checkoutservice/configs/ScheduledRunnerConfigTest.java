f9ab24fdc1867a7077d26bae6bea4fb61084c1ed
TrungTho <trantrungtho296@gmail.com>
2025-05-23 18:03:37 +0700

    test(checkout): enhance test for CheckoutProcessingCoordinator

--- services/checkout/src/test/java/com/saga/playground/checkoutservice/configs/ScheduledRunnerConfigTest.java
+++ services/checkout/src/test/java/com/saga/playground/checkoutservice/configs/ScheduledRunnerConfigTest.java
@@ -1,5 +1,6 @@
 package com.saga.playground.checkoutservice.configs;
 
+import com.saga.playground.checkoutservice.constants.ErrorConstant;
 import com.saga.playground.checkoutservice.constants.WorkerConstant;
 import com.saga.playground.checkoutservice.domains.entities.TransactionalInboxOrder;
 import com.saga.playground.checkoutservice.workers.checkout.CheckoutHelper;
@@ -52,9 +53,12 @@ class ScheduledRunnerConfigTest {
             .pullOrders();
         Mockito.verify(checkoutProcessingWorker, Mockito.times(0))
             .processCheckout(Mockito.any());
+        Mockito.verify(checkoutHelper, Mockito.times(0))
+            .postCheckoutProcess(Mockito.any());
         Mockito.verify(spyThreadPool, Mockito.times(0))
             .execute(Mockito.any());
     }
+
 
     @Test
     void testRun_NonEmptyOrder(CapturedOutput output) {
@@ -81,8 +85,49 @@ class ScheduledRunnerConfigTest {
         Mockito.verify(spyThreadPool, Mockito.times(numberOfRecords))
             .execute(Mockito.any());
         mockOrders.forEach(
-            order -> Mockito.verify(checkoutProcessingWorker, Mockito.times(1))
-                .processCheckout(order.getOrderId()));
+            order -> {
+                Mockito.verify(checkoutProcessingWorker, Mockito.times(1))
+                    .processCheckout(order.getOrderId());
+                Mockito.verify(checkoutHelper, Mockito.times(1))
+                    .postCheckoutProcess(order.getOrderId());
+            });
     }
 
+    @Test
+    void testRun_NoPostCheckoutInCrash(CapturedOutput output) {
+        var spyThreadPool = Mockito.spy(threadPool);
+        int numberOfRecords = 10;
+        var mockOrders = Instancio.ofList(TransactionalInboxOrder.class)
+            .size(numberOfRecords)
+            .create();
+
+        Mockito.when(checkoutProcessingWorker.pullOrders())
+            .thenReturn(mockOrders);
+        Mockito.doThrow(new RuntimeException(""))
+            .when(checkoutProcessingWorker).processCheckout(Mockito.any());
+
+        var checkoutCoordinator =
+            new ScheduledRunnerConfig.CheckoutProcessingCoordinator(
+                spyThreadPool, checkoutProcessingWorker, checkoutHelper);
+
+        Assertions.assertDoesNotThrow(checkoutCoordinator::run);
+
+        Assertions.assertTrue(output.toString().contains("Retrieving orders"));
+        Assertions.assertTrue(output.toString().contains("Finish submitting %d orders for checking out"
+            .formatted(mockOrders.size())));
+        Mockito.verify(checkoutProcessingWorker, Mockito.times(1))
+            .pullOrders();
+        Mockito.verify(spyThreadPool, Mockito.times(numberOfRecords))
+            .execute(Mockito.any());
+        mockOrders.forEach(
+            order -> {
+                Mockito.verify(checkoutProcessingWorker, Mockito.times(1))
+                    .processCheckout(order.getOrderId());
+            });
+
+        // regardless how many failed checkout process -> no post checkout happen
+        Mockito.verify(checkoutHelper, Mockito.times(0))
+            .postCheckoutProcess(Mockito.any());
+        Assertions.assertTrue(output.toString().contains(ErrorConstant.CODE_UNHANDED_ERROR));
+    }
 }
