6ca27798adfe658abbf37c5c24827b3714f44c6e
TrungTho <trantrungtho296@gmail.com>
2025-06-02 11:17:50 +0700

    chore(local): move debezium-bootstrapping.sh to bootstrapping.sh

--- deploys/bootstrap.sh
+++ deploys/bootstrap.sh
@@ -6,18 +6,13 @@ set -m
 #### kafka bootstrapping ####
 #############################
 
-HOST=kafka-1:29092
+KAFKA_HOST=kafka-1:29092
 NUMBER_PARTITION=4
 NUMBER_RF=2
 CHECKOUT_SUCCESSFUL=db.saga_playground.checkout.successful
 CHECKOUT_FAILED=db.saga_playground.checkout.failed
 FULFILLMENT_SUCCESSFUL=db.saga_playground.fulfillment.successful
 FULFILLMENT_FAILED=db.saga_playground.fulfillment.failed
-
-# echo "=====================";
-# echo "=====Start Kafka=====";
-# echo "=====================";
-# /etc/confluent/docker/run & KAFKA_PID=$! # run kafka in background in order not to block the below bootstrap steps
 
 sleep 5s; # to make sure kafka server is ready to handle CLI request
 echo "=====================";
@@ -27,9 +22,9 @@ echo "=====================";
 # check if topics exist -> create topics if needed
 
 # CHECKOUT_SUCCESSFUL topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $CHECKOUT_SUCCESSFUL &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $CHECKOUT_SUCCESSFUL &> /dev/null; then
     echo "topic $CHECKOUT_SUCCESSFUL does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $CHECKOUT_SUCCESSFUL \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
@@ -39,9 +34,9 @@ else
 fi;
 
 # CHECKOUT_FAILED topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $CHECKOUT_FAILED &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $CHECKOUT_FAILED &> /dev/null; then
     echo "topic $CHECKOUT_FAILED does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $CHECKOUT_FAILED \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
@@ -51,9 +46,9 @@ else
 fi;
     
 # FULFILLMENT_SUCCESSFUL topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $FULFILLMENT_SUCCESSFUL &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $FULFILLMENT_SUCCESSFUL &> /dev/null; then
     echo "topic $FULFILLMENT_SUCCESSFUL does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $FULFILLMENT_SUCCESSFUL \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
@@ -63,9 +58,9 @@ else
 fi;
 
 # FULFILLMENT_FAILED topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $FULFILLMENT_FAILED &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $FULFILLMENT_FAILED &> /dev/null; then
     echo "topic $FULFILLMENT_FAILED does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $FULFILLMENT_FAILED \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
@@ -78,10 +73,22 @@ echo "==========================";
 echo "Finish Kafka Bootstrapping";
 echo "==========================";
 
-# # bring the background running Kafka to Foreground to prevent container stop
-# echo "=====================";
-# echo "=======FG Kafka======";
-# echo "=====================";
-# echo "Wait for PID" $KAFKA_PID;
-# wait $KAFKA_PID
-# echo "Exit status: $?"
+
+##############################
+### debezium bootstrapping ###
+##############################
+
+DEBEZIUM_HOST=debezium-connector:8083
+
+
+echo "=====================";
+echo "===Retry connection===";
+echo "=====================";
+curl -f --retry 10 --retry-delay 5 --retry-max-time 40 http://$DEBEZIUM_HOST/connectors; 
+
+echo "=====================";
+echo "=Init Order connector=";
+echo "=====================";
+
+curl -X POST -H "Content-Type: application/json" -d @/saga-order-cdc-config.json http://$DEBEZIUM_HOST/connectors
+
