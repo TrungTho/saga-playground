7a4df08610e9a6fb3a13b247072b99c443d6ebf2
TrungTho <41388666+TrungTho@users.noreply.github.com>
2025-06-02 20:34:56 +0700

    chore(deploy): refactor bootstrapping containers (#25)
    * chore(local): move kafka-bootstrapping.sh to init container instead of running in main container
    
    * chore(local): move debezium-bootstrapping.sh to bootstrapping.sh
    
    * chore(ci): add workflow to verify local set up
    
    * chore(ci): push example env file to use in ci
    
    * chore(ci): fix ci
    
    * chore(ci): try to fix ci
    
    * chore(ci): remove local ci

--- deploys/bootstrap.sh
+++ deploys/bootstrap.sh
@@ -2,18 +2,17 @@
 set -e
 set -m
 
-HOST=localhost:29092
+#############################
+#### kafka bootstrapping ####
+#############################
+
+KAFKA_HOST=kafka-1:29092
 NUMBER_PARTITION=4
 NUMBER_RF=2
 CHECKOUT_SUCCESSFUL=db.saga_playground.checkout.successful
 CHECKOUT_FAILED=db.saga_playground.checkout.failed
 FULFILLMENT_SUCCESSFUL=db.saga_playground.fulfillment.successful
 FULFILLMENT_FAILED=db.saga_playground.fulfillment.failed
-
-echo "=====================";
-echo "=====Start Kafka=====";
-echo "=====================";
-/etc/confluent/docker/run & KAFKA_PID=$! # run kafka in background in order not to block the below bootstrap steps
 
 sleep 5s; # to make sure kafka server is ready to handle CLI request
 echo "=====================";
@@ -23,53 +22,73 @@ echo "=====================";
 # check if topics exist -> create topics if needed
 
 # CHECKOUT_SUCCESSFUL topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $CHECKOUT_SUCCESSFUL &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $CHECKOUT_SUCCESSFUL &> /dev/null; then
     echo "topic $CHECKOUT_SUCCESSFUL does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $CHECKOUT_SUCCESSFUL \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
+    echo "topic $CHECKOUT_SUCCESSFUL was successfully created";
 else
     echo "topic $CHECKOUT_SUCCESSFUL already existed";
 fi;
 
 # CHECKOUT_FAILED topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $CHECKOUT_FAILED &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $CHECKOUT_FAILED &> /dev/null; then
     echo "topic $CHECKOUT_FAILED does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $CHECKOUT_FAILED \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
+    echo "topic $CHECKOUT_FAILED was successfully created";
 else
     echo "topic $CHECKOUT_FAILED already existed";
 fi;
     
 # FULFILLMENT_SUCCESSFUL topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $FULFILLMENT_SUCCESSFUL &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $FULFILLMENT_SUCCESSFUL &> /dev/null; then
     echo "topic $FULFILLMENT_SUCCESSFUL does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $FULFILLMENT_SUCCESSFUL \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
+    echo "topic $FULFILLMENT_SUCCESSFUL was successfully created";
 else
     echo "topic $FULFILLMENT_SUCCESSFUL already existed";
 fi;
 
 # FULFILLMENT_FAILED topic
-if ! kafka-topics --bootstrap-server $HOST --describe --topic $FULFILLMENT_FAILED &> /dev/null; then
+if ! kafka-topics --bootstrap-server $KAFKA_HOST --describe --topic $FULFILLMENT_FAILED &> /dev/null; then
     echo "topic $FULFILLMENT_FAILED does not exist";
-    kafka-topics --bootstrap-server $HOST \
+    kafka-topics --bootstrap-server $KAFKA_HOST \
         --create --topic $FULFILLMENT_FAILED \
         --replication-factor $NUMBER_RF \
         --partitions $NUMBER_PARTITION;
+    echo "topic $FULFILLMENT_FAILED was successfully created";
 else
     echo "topic $FULFILLMENT_FAILED already existed";
 fi;
 
-# bring the background running Kafka to Foreground to prevent container stop
+echo "==========================";
+echo "Finish Kafka Bootstrapping";
+echo "==========================";
+
+
+##############################
+### debezium bootstrapping ###
+##############################
+
+DEBEZIUM_HOST=debezium-connector:8083
+
+
 echo "=====================";
-echo "=======FG Kafka======";
+echo "===Retry connection===";
 echo "=====================";
-echo "Wait for PID" $KAFKA_PID;
-wait $KAFKA_PID
-echo "Exit status: $?"
+curl -f --retry 10 --retry-delay 5 --retry-max-time 40 http://$DEBEZIUM_HOST/connectors; 
+
+echo "=====================";
+echo "=Init Order connector=";
+echo "=====================";
+
+curl -X POST -H "Content-Type: application/json" -d @/saga-order-cdc-config.json http://$DEBEZIUM_HOST/connectors
+
